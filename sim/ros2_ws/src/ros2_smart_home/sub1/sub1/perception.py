7c754e1af3f211a8ec33530bfb651666f9fd231c
정한균 <j1113019@naver.com>
2025-03-14 12:34:01 +0900

    S12P21E102-358 feat: 이미지 처리 노드(IMGParser) 기능 구현 및 주석 정리
    - ROS2 CompressedImage 메시지를 수신하여 OpenCV에서 처리 가능하도록 변환
    - BGR 이미지를 그레이스케일로 변환하는 기능 구현
    - 원하는 해상도로 이미지 리사이징 기능 구현
    - 이미지 출력 (BGR 원본, Grayscale, Resized) 기능 구현
    - 기존 주석을 기능 중심 설명으로 정리하여 가독성 향상

--- sim/ros2_ws/src/ros2_smart_home/sub1/sub1/perception.py
+++ sim/ros2_ws/src/ros2_smart_home/sub1/sub1/perception.py
@@ -7,27 +7,19 @@ from rclpy.node import Node
 
 from sensor_msgs.msg import CompressedImage
 
-# image parser 노드는 이미지를 받아서 opencv 의 imshow로 윈도우창에 띄우는 역할을 합니다.
-# 이를 resize나 convert를 사용하여 이미지를 원하는대로 바꿔보세요.
-
-# 노드 로직 순서
-# 1. image subscriber 생성
-# 2. 카메라 콜백함수에서 compressed image 디코딩
-# 3. 이미지 색 채널을 gray scale로 컨버팅
-# 4. 이미지 resizing
-# 5. 이미지 imshow
-
+# IMGParser 노드는 터틀봇의 카메라로부터 받은 이미지를 처리하는 역할을 함
+# - ROS2의 CompressedImage 메시지를 수신하여 OpenCV에서 처리 가능한 형식으로 변환
+# - 원본 BGR 이미지를 출력
+# - 그레이스케일 변환 후 출력
+# - 이미지 크기를 조정하여 출력
 
 class IMGParser(Node):
 
     def __init__(self):
         super().__init__(node_name='image_convertor')
 
-        # 로직 1. image subscriber 생성
-        ## 아래와 같이 subscriber가 
-        ## 미리 정의된 토픽 이름인 '/image_jpeg/compressed' 에서
-        ## CompressedImage 메시지를 받도록 설정된다.
-
+        # 카메라 이미지 데이터를 구독하는 ROS2 Subscriber 생성
+        # '/image_jpeg/compressed' 토픽에서 CompressedImage 메시지를 수신하여 처리
         self.subscription = self.create_subscription(
             CompressedImage,
             '/image_jpeg/compressed',
@@ -36,49 +28,51 @@ class IMGParser(Node):
         
 
     def img_callback(self, msg):
+        """
+        카메라에서 받은 압축된 이미지 데이터를 디코딩하여 OpenCV에서 사용 가능한 형식으로 변환 후 출력.
+        1. bytes 데이터를 uint8 배열로 변환
+        2. OpenCV를 이용해 BGR 형식의 이미지로 디코딩
+        3. BGR 이미지를 그레이스케일로 변환
+        4. 그레이스케일 이미지를 원하는 해상도로 리사이징
+        5. 변환된 이미지를 화면에 출력
+        """
 
-        # 로직 2. 카메라 콜백함수에서 이미지를 클래스 내 변수로 저장
-        ## msg.data 는 bytes로 되어 있고 이를 uint8로 바꾼 다음
-        ## cv2 내의 이미지 디코딩 함수로 bgr 이미지로 바꾸세요.        
-
+        # 압축된 이미지 데이터를 uint8 배열로 변환
         np_arr = np.frombuffer(msg.data, np.uint8)
+
+        # OpenCV를 이용하여 BGR 이미지로 디코딩
         img_bgr = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-        '''
-        로직 3. 이미지 색 채널을 gray scale로 컨버팅
-        cv2. 내의 이미지 색 채널 컨터버로 bgr 색상을 gary scale로 바꾸십시오.
-
-        img_gray = 
-
-        '''
-
-        '''
-        로직 4. 이미지 resizing
-        cv2를 사용해서 이미지를 원하는 크기로 바꿔보십시오.
-
-        img_resize = 
-        '''
-
-        # 로직 5. 이미지 출력 (cv2.imshow)       
         
-        cv2.imshow("img_bgr", img_bgr)
-        # cv2.imshow("img_gray", img_gray)
-        # cv2.imshow("resize and gray", img_resize)       
+        # BGR 이미지를 그레이스케일로 변환
+        img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+
+        # 이미지를 원하는 크기로 리사이징 (INTER_AREA 보간법 사용)
+        img_resize = cv2.resize(img_gray, (1920, 1080), interpolation=cv2.INTER_AREA)      
         
+        # 변환된 이미지 출력
+        cv2.imshow("img_bgr", img_bgr) # 원본 BGR 이미지
+        # cv2.imshow("img_gray", img_gray) # 변환된 그레이스케일 이미지
+        # cv2.imshow("resize and gray", img_resize) # 리사이징된 이미지
+        
+        # OpenCV 창을 유지 (키 입력 대기)
         cv2.waitKey(1)
 
 
 def main(args=None):
-
-    ## 노드 초기화 : rclpy.init 은 node의 이름을 알려주는 것으로, ROS master와 통신을 가능하게 해줍니다.
+    """
+    ROS2 노드를 초기화하고 실행하는 메인 함수.
+    - rclpy.init(): ROS2 노드 초기화
+    - IMGParser 노드 인스턴스 생성
+    - rclpy.spin(): 노드가 종료될 때까지 실행 유지
+    """
     rclpy.init(args=args)
 
-    ## 메인에 돌릴 노드 클래스 정의 
+    # IMGParser 노드 실행 
     image_parser = IMGParser()
 
-    ## 노드 실행 : 노드를 셧다운하기 전까지 종료로부터 막아주는 역할을 합니다
+    # ROS2 노드를 지속적으로 실행 (종료될 때까지)
     rclpy.spin(image_parser)
 
 
 if __name__ == '__main__':
-
     main()
