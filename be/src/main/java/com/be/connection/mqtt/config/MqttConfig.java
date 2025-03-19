25c0b7b215bc5f4c0c2600c1cb5757bac807f98f
김지홍 <rokepj2696@gmail.com>
2025-03-19 12:46:14 +0900

    hotfix: 설명 주석 추가

--- be/src/main/java/com/be/connection/mqtt/config/MqttConfig.java
+++ be/src/main/java/com/be/connection/mqtt/config/MqttConfig.java
@@ -17,41 +17,57 @@ import org.springframework.messaging.MessageChannel;
 
 import jakarta.websocket.MessageHandler;
 
+/**
+ * MQTT 관련 설정을 관리하는 클래스입니다.
+ */
 @Configuration
 public class MqttConfig {
 
-	@Value("${spring.mqtt.broker-url}")
+	@Value("${spring.mqtt.broker-url}")//mqtt 보내기위한 브로커 서버 url - 추후 수정 필요
 	private String brokerUrl;
 
-	@Value("${spring.mqtt.client-id}")
+	@Value("${spring.mqtt.client-id}")//mqtt 클라이언트의 고유 아이디 id
 	private String clientId;
 
-	@Value("${spring.mqtt.topic}")
+	@Value("${spring.mqtt.topic}")//MQTT 메시지를 발행할 토픽입니다. 여러 클라이언트가 동일한 토픽을 구독할 수 있습니다.
 	private String topic;
 
+	// MQTT 클라이언트 팩토리 빈 생성 . 해당 팩토리는 MQTT 클라이언트 연결 옵션을 설정합니다.
 	@Bean
 	public MqttPahoClientFactory mqttClientFactory() {
 		DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
 		MqttConnectOptions options = new MqttConnectOptions();
-		options.setServerURIs(new String[]{brokerUrl});
-		factory.setConnectionOptions(options);
+		options.setServerURIs(new String[]{brokerUrl});//MQTT 브로커 서버 주소
+		factory.setConnectionOptions(options);//연결 옵션 설정 관련
 		return factory;
 	}
 
+	/**
+	 * MQTT 메시지를 발행하는 핸들러 빈
+	 * 이 핸들러는 mqttOutboundChannel에서 수신한 메시지를 MQTT 브로커로 전송.
+	 */
 	@Bean
-	@ServiceActivator(inputChannel = "mqttOutboundChannel")
+	@ServiceActivator(inputChannel = "mqttOutboundChannel")// mqttOutboundChannel에서 메시지를 처리
 	public MqttPahoMessageHandler mqttOutbound() {
 		MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler(clientId, mqttClientFactory());
-		messageHandler.setAsync(true);
-		messageHandler.setDefaultTopic(topic);
+		messageHandler.setAsync(true);// 비동기 처리 활성화
+		messageHandler.setDefaultTopic(topic);// 기본 토픽 설정
 		return messageHandler;
 	}
 
+	/**
+	 * MQTT 메시지를 발행하기 위한 채널을 생성
+	 * 이 채널은 MyGateway 인터페이스를 통해 메시지를 수신합니다.
+	 */
 	@Bean
 	public MessageChannel mqttOutboundChannel() {
 		return new DirectChannel();
 	}
 
+	/**
+	 * MQTT 메시지를 발행하기 위한 게이트웨이 인터페이스
+	 * 이 인터페이스를 통해 메시지를 mqttOutboundChannel로 전송합니다.
+	 */
 	@MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
 	public interface MyGateway {
 		void sendToMqtt(String data);
