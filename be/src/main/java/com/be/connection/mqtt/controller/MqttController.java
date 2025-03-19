b3bad6629b390da22d6521ef1226e2164d608ff2
김지홍 <rokepj2696@gmail.com>
2025-03-19 12:35:16 +0900

    S12P21E102-406,407 feat: mqtt spring boot 에 기본 설정 완료. mosquitto 동작 및 메시지 처리 확인

--- be/src/main/java/com/be/connection/mqtt/controller/MqttController.java
+++ be/src/main/java/com/be/connection/mqtt/controller/MqttController.java
@@ -1,4 +1,25 @@
 package com.be.connection.mqtt.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.be.connection.mqtt.config.MqttConfig;
+
+@RestController
 public class MqttController {
+
+	private final MqttConfig.MyGateway myGateway;
+
+	@Autowired
+	public MqttController(MqttConfig.MyGateway myGateway) {
+		this.myGateway = myGateway;
+	}
+
+	@PostMapping("/mqtt")
+	public String sendMqttMessage(@RequestBody String message) {
+		myGateway.sendToMqtt(message);
+		return "Message sent to MQTT topic.";
+	}
 }
