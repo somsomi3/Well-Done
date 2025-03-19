4ad1cf90c2c3d31ff702d28f593933e0decb6ab1
김지홍 <rokepj2696@gmail.com>
2025-03-19 15:41:35 +0900

    S12P21E102-407 feat: mqtt 테스트 성공 및 기능 주석 추가 + service 활용하도록 controller 구문 수정

--- be/src/main/java/com/be/connection/mqtt/service/MqttService.java
+++ be/src/main/java/com/be/connection/mqtt/service/MqttService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 
 import com.be.connection.mqtt.config.MqttConfig;
 
+/**
+ * MQTT 메시지를 발행및 처리하는 서비스 클래스입니다.
+ */
 @Service
 public class MqttService {
 
@@ -13,6 +16,10 @@ public class MqttService {
 		this.myGateway = myGateway;
 	}
 
+	/**
+	 * MQTT 메시지를 발행하는 메서드입니다.
+	 * 이 메서드는 "Hello MQTT!"라는 메시지를 MQTT 브로커로 전송합니다.
+	 */
 	public void process() {
 		String msg = "Hello MQTT!";
 		myGateway.sendToMqtt(msg);
