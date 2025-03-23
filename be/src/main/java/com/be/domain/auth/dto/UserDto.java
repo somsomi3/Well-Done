934108079b6d4cd54c889667a8b6925c5517efb4
전민경 <chfhddlfhqls@naver.com>
2025-03-23 23:25:05 +0900

    S12P21E102-389 fix: 로그인 기능 관련 필터 추가

--- be/src/main/java/com/be/domain/auth/dto/UserDto.java
+++ be/src/main/java/com/be/domain/auth/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.be.domain.auth.dto;
 
+import com.be.db.entity.User;
 import lombok.*;
 
 @Getter
@@ -12,7 +13,15 @@ public class UserDto {
     private String username;       // 사용자 이름
     private String email;          // 이메일 주소
     private String password;       // 비밀번호 (암호화된 형태로 저장되어야 함)
-    private String role;           // 사용자 역할 (예: ADMIN, USER)
+//    private String role;           // 사용자 역할 (예: ADMIN, USER)
+
+    public UserDto(User user) {
+        this.userid = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.password = user.getPassword(); // password 제외해도 됨
+
+    }
 
     // 필요한 경우에만 추가 필드들 (예: 사용자 상태, 전화번호 등)
 }
