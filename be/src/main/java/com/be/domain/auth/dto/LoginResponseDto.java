05732f97be33d399f46e8de3e10a994ad1018831
전민경 <chfhddlfhqls@naver.com>
2025-03-23 22:12:37 +0900

    S12P21E102-390 feat: 로그인/회원가입 응답값 설정

---
+++ be/src/main/java/com/be/domain/auth/dto/LoginResponseDto.java
@@ -0,0 +1,13 @@
+package com.be.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+    private String email;
+    private String username;
+}
---
+++ be/src/main/java/com/be/domain/auth/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.be.domain.auth.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "아이디는 필수입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+}
+
