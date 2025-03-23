40c2a7941f6c6d5f95ee2cea4fd4b8416f12b3d1
김동현 <edwin.dkim@gmail.com>
2025-03-23 14:07:03 +0900

    S12P21E102 feat: 프로젝트 설정 변경+수정,- dockerfile 작성, docker-compose.yml 작성- 실제로 작동하는지 확인 완료

--- be/src/main/java/com/be/config/security/SecurityConfig.java
+++ be/src/main/java/com/be/config/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
 			.csrf(csrf -> csrf.disable()) // CSRF 비활성화
 			.formLogin(withDefaults()) // 기본 로그인 폼 사용
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/mqtt").permitAll() // MQTT 관련 엔드포인트는 인증 없이 접근 가능
+				.requestMatchers("/", "/mqtt").permitAll() // 루트 경로와 MQTT 엔드포인트는 인증 없이 접근 가능
 				.anyRequest().authenticated()); // 다른 요청은 인증 필요
 		return http.build();
 	}
