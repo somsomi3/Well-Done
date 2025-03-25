3c87813d8cf2580d3b1cb3058263e0347eaace54
김동현 <edwin.dkim@gmail.com>
2025-03-25 16:55:20 +0900

    S12P21E102 feat : SpringSecurity설정 추가.

--- be/src/main/java/com/be/config/security/SecurityConfig.java
+++ be/src/main/java/com/be/config/security/SecurityConfig.java
@@ -101,6 +101,7 @@ public class SecurityConfig {
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(
+								"/",
 								"/swagger-ui/**",
 								"/v3/api-docs/**",
 								"/swagger-resources/**",
