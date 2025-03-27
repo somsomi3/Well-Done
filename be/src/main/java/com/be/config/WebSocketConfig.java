3e04a49f9b945cbe17f2335bea8b4ad6b5209f7b
전민경 <chfhddlfhqls@naver.com>
2025-03-27 10:12:58 +0900

    S12P21E102-395 feat: 인증인터셉터 및 핸들러 등록

--- be/src/main/java/com/be/config/WebSocketConfig.java
+++ be/src/main/java/com/be/config/WebSocketConfig.java
@@ -1,11 +1,16 @@
 package com.be.config;
 
+import com.be.domain.auth.WebSocketAuthInterceptor;
+import com.be.domain.robot.SimulatorSocketHandler;
+import com.be.domain.robot.UserSocketHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.WebSocketHttpRequestHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -13,14 +18,27 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-//    private final SimulatorSocketHandler simulatorSocketHandler;
-//    private final UserSocketHandler userSocketHandler;
+    private final SimulatorSocketHandler simulatorSocketHandler;
+    private final UserSocketHandler userSocketHandler;
+
+    // 인증을 위한 인터셉터 추가
+    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(simulatorSocketHandler, "/ws/simulator").setAllowedOrigins("*");
-//        registry.addHandler(userSocketHandler, "/ws/user").setAllowedOrigins("*");
-    }
+        log.info("WebSocket Handlers 등록");
 
+        registry.addHandler(simulatorSocketHandler, "/ws/simulator")
+                .setAllowedOrigins("*")
+                .addInterceptors(webSocketAuthInterceptor);
+
+        log.info("simulatorSocketHandler 등록 (path: /ws/simulator)");
+
+        registry.addHandler(userSocketHandler, "/ws/user")
+                .setAllowedOrigins("*")
+                .addInterceptors(webSocketAuthInterceptor);
+
+        log.info("userSocketHandler 등록(path: /ws/user)");
+    }
 
 }
