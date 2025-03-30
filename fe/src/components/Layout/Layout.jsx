34ff5962f1910fcbe82678799f044db975f708cb
유지웅 <ung03@naver.com>
2025-03-31 06:56:21 +0900

    S12P21E102-562 feat: 레이아웃 라우팅

--- fe/src/components/Layout/Layout.jsx
+++ fe/src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
+// 레이아웃 라우팅팅
 function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
