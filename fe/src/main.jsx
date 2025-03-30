943887c0374387e0ca00238adf26c3e052fcf80e
유지웅 <ung03@naver.com>
2025-03-31 06:36:23 +0900

    S12P21E102-556 feat: app, main 컴포넌트 수정

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Modal from 'react-modal'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// 앱 요소 설정 (접근성을 위해 필수)
+Modal.setAppElement('#root');
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
