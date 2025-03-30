eb6629d3588c387f522d8e63f719b2b9fec27507
전민경 <chfhddlfhqls@naver.com>
2025-03-30 23:51:41 +0900

    S12P21E102-397 fix: 관리자 기능(레포짓토리) 어노테이션추가

--- be/src/main/java/com/be/db/repository/RoleRepository.java
+++ be/src/main/java/com/be/db/repository/RoleRepository.java
@@ -2,8 +2,11 @@ package com.be.db.repository;
 
 import com.be.db.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 	Optional<Role> findByName(Role.RoleName name);
 }
