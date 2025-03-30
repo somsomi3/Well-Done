533864276a5421f8f29215bd67860938aba3726e
전민경 <chfhddlfhqls@naver.com>
2025-03-30 22:03:42 +0900

    S12P21E102-397 feat: 관리자 기능 구현(레포짓토리)

---
+++ be/src/main/java/com/be/db/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.be.db.repository;
+
+import com.be.db.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(Role.RoleName name);
+}
