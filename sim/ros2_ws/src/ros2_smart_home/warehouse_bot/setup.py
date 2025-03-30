8e4772f307b7d9f51ce66692075c324f18dc813c
정한균 <j1113019@naver.com>
2025-03-30 19:25:45 +0900

    S12P21E102-524 feat: ROS2 통합 패키지 warehouse_bot 생성
    - ROS2 Python 패키지 warehouse_bot 기본 생성
    - setup.py, package.xml, __init__.py 구성
    - launch, msg, scripts 디렉토리 초기화
    - SLAM 및 자율 기능 통합을 위한 기반 마련

---
+++ sim/ros2_ws/src/ros2_smart_home/warehouse_bot/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = "warehouse_bot"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/warehouse_bot"]),
+        ("share/" + package_name, ["package.xml"]),
+        # launch 파일 자동 포함
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        # config 파일 포함
+        (os.path.join("share", package_name, "config"), glob("config/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="SSAFY12thE102",
+    maintainer_email="j1113019@naver.com",
+    description="Warehouse autonomous robot core nodes",
+    license="MIT",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "main_launch = warehouse_bot.main_launch:main",
+        ],
+    },
+)
