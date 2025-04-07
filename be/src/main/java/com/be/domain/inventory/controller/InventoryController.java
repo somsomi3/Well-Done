f7c43f4097caf1b22f20ac9db36fb138468c1869
전민경 <chfhddlfhqls@naver.com>
2025-04-07 11:13:11 +0900

    S12P21E102-625 fix: 재고 관련 기능 수정

--- be/src/main/java/com/be/domain/inventory/controller/InventoryController.java
+++ be/src/main/java/com/be/domain/inventory/controller/InventoryController.java
@@ -1,9 +1,11 @@
 package com.be.domain.inventory.controller;
 
 import com.be.db.entity.Inventory;
+import com.be.domain.inventory.dto.InventoryDto;
 import com.be.domain.inventory.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +21,8 @@ public class InventoryController {
 
     // 전체 재고 목록 조회
     @GetMapping("")
-    public ResponseEntity<List<Inventory>> getAllInventory() {
-        List<Inventory> inventories = inventoryService.findAll();
-        return ResponseEntity.ok(inventories);
+    public ResponseEntity<List<InventoryDto>> getAllInventory() {
+        return ResponseEntity.ok(inventoryService.findAllDto());
     }
 
     // 단건 조회
@@ -32,16 +33,18 @@ public class InventoryController {
     }
 
     // 수량 조정
+
     @PostMapping("/{id}/adjust")
     public ResponseEntity<Inventory> adjustInventory(
             @PathVariable Long id,
             @RequestParam int amount,
-            @AuthenticationPrincipal UserDetails userDetails) {
+            Authentication authentication) {
 
-        String updatedBy = userDetails.getUsername(); // 로그인한 유저 이름 가져오기
+        String updatedBy = authentication.getName(); // UserDto에서 getUsername()을 가져옴
         Inventory updated = inventoryService.updateStock(id, amount, updatedBy);
         return ResponseEntity.ok(updated);
     }
+
 
     @PostMapping
     public ResponseEntity<Inventory> createInventory(@RequestBody Inventory inventory) {
