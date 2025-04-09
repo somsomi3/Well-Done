90af7454b4fb23bc18e9b1e15e8d8a5bb25d21ed
ungit003 <ungit003@gmail.com>
2025-04-10 04:36:33 +0900

    feat: 헤더 푸터 고정

--- fe/src/components/Layout/Layout.jsx
+++ fe/src/components/Layout/Layout.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import ErrorBoundary from '../ErrorBoundary';
 
 function Layout({ children }) {
   return (
-    <ErrorBoundary>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          {children}
-        </main>
-        <Footer />
-        {/* 토스트 컨테이너는 App.jsx에서만 렌더링하므로 여기서는 제거 */}
-      </div>
-    </ErrorBoundary>
+    <div className="flex flex-col h-screen overflow-hidden">
+      <Header className="sticky top-0 z-50" />
+      <main className="flex-grow overflow-auto">
+        {children}
+      </main>
+      <Footer className="sticky bottom-0 z-50" />
+    </div>
   );
 }
 
